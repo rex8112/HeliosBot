50ab988c8fbe1463e41696aa938228c8afdd39d2
Riley Winkler <rex8112@gmail.com>
2021-08-28 14:08:22 -0500

    Update configLoader.py

--- tools/configLoader.py
+++ tools/configLoader.py
@@ -13,9 +13,19 @@ except IOError as e:
   logger.critical('config.ini not found, generating one now. Please fill it in.')
   config['settings']['token'] = ''
   config['settings']['ownerID'] = '180067685986467840'
+  config['database']['db_name'] = ''
+  config['database']['db_user'] = ''
+  config['database']['db_password'] = ''
+  config['database']['db_host'] = ''
+  config['database']['db_port'] = ''
   config.write()
   sys.exit()
 
 class settings:
   token = config['settings']['token']
   owner = config['settings']['ownerID'].as_int()
+  db_name = config['database']['db_name']
+  db_user = config['database']['db_user']
+  db_password = config['database']['db_password']
+  db_host = config['database']['db_host']
+  db_port = config['database']['db_port'].as_int()
