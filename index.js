e66252465e85e7b29b7c2ee84acabea3fe393633
Riley Winkler <rex8112@gmail.com>
2021-08-28 22:53:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const { sequelize } = require('./tools/database.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
@@ -14,6 +25,22 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     console.log('Ready!');
+    sequelize.sync();
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
 });
 
 client.login(token);
