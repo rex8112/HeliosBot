824dcb52ef93f1b20737cd0ff452ba19c3f7f04a
Riley Winkler <rex8112@gmail.com>
2021-09-05 20:04:01 -0500

    Reduce clutter with archive

--- commands/archive.js
+++ commands/archive.js
@@ -4,23 +4,22 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('archive')
         .setDescription('Either show or hide the archive category')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('show')
-                .setDescription('Shows the archive category'))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('hide')
-                .setDescription('hides the archive category')),
+        .addStringOption(option =>
+            option
+                .setName('option')
+                .setDescription('Whether to show/hide the archive category.')
+                .setRequired(true)
+                .addChoice('Show', 'show')
+                .addChoice('Hide', 'hide')),
     async execute(interaction) {
         const server = interaction.client.servers.get(interaction.guild.id);
-        if (interaction.options.getSubcommand() === 'show') {
+        if (interaction.options.getString('option') === 'show') {
             if (await server.showArchive(interaction.member)) {
                 interaction.reply({ content: 'The archive category has been shown.', ephemeral: true });
             } else {
                 interaction.reply({ content: 'The archive category does not exist.', ephemeral: true });
             }
-        } else if (interaction.options.getSubcommand() === 'hide') {
+        } else if (interaction.options.getString('option') === 'hide') {
             if (await server.hideArchive(interaction.member)) {
                 interaction.reply({ content: 'The archive category has been hidden.', ephemeral: true });
             } else {
