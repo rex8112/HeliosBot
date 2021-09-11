65a497f8a2ccfe3035154f2235367c56cc547b2e
Riley Winkler <rex8112@gmail.com>
2021-09-11 18:18:50 -0500

    Convert point system

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -22,7 +22,7 @@ module.exports = {
             for (let i = 0; i < decks.length; i++) {
                 let add = false;
                 const deck = decks[i];
-                let stringToAdd = `${position}. ${userMention(deck.member.id)}: ${deck.totalPoints} Points`;
+                let stringToAdd = `${position}. ${userMention(deck.member.id)}: ${deck.earnedPoints} Earned Points`;
                 if (i < 5) add = true;
                 if (deck.member.id === interaction.member.id) {
                     stringToAdd = bold(stringToAdd);
