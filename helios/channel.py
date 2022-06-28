e291249c538b9104496d14efd6fb4c301f81cf14
Riley Winkler <rex8112@gmail.com>
2022-06-28 15:56:33 -0500

    Privatize deserialize method and take data in init

--- helios/channel.py
+++ helios/channel.py
@@ -1,44 +1,31 @@
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from discord.abc import PrivateChannel
-    from helios import ChannelManager, HeliosBot, IdMismatchError
-    from discord.types.threads import Thread
-    from discord.types.channel import GuildChannel
+    from helios import ChannelManager, HeliosBot
 
 
 class Channel:
     channel_type = 'Basic'
 
     def __init__(self,
-                 bot: 'HeliosBot',
                  manager: 'ChannelManager',
-                 *,
-                 channel_id: int = None,
-                 channel: Optional[Union['GuildChannel', 'Thread', 'PrivateChannel']] = None):
+                 data: dict):
         """
         A special channel class that will hold all the important functions of that class
-        :param bot: The bot being run
         :param manager: The channel manager handling this channel
-        :param channel_id: The id of a discord channel, this will be fetched later
-        :param channel: The discord channel of this channel
+        :param data: Channel data dict
         """
-        self.bot = bot
+        self.bot: 'HeliosBot' = manager.bot
         self.manager = manager
         self.channel = None
         self.settings = {}
         self.flags = []
         self._id = None
 
-        if channel:
-            self.channel = channel
-            self._id = channel.id
-        elif channel_id:
-            self._id = channel_id
-        else:
-            raise ValueError('_id or channel required.')
+        self._id = data['id']
+        self.channel = self.bot.get_channel(self._id)
 
-        self.loaded = False
+        self._deserialize(data)
 
     @property
     def id(self):
@@ -51,14 +38,18 @@ class Channel:
     def server(self):
         return self.manager.server
 
-    async def deserialize(self, data: dict) -> None:
-        data_id = data.get('id')
-        if data_id != self.id:
-            raise IdMismatchError('ID of data does not match channel ID')
+    @property
+    def alive(self):
+        if self.bot.get_channel(self.id):
+            return True
+        else:
+            return False
 
+    def _deserialize(self, data: dict) -> None:
+        if self.channel_type != data.get('type'):
+            raise TypeError(f'Channel data is of type {data.get("type")} not {self.channel_type}')
         self.settings = data.get('settings')
         self.flags = data.get('flags')
-        self.loaded = True
 
     def serialize(self) -> dict:
         return {
