e4957d684515ee4b02842426e0b7f2163a0d692a
Riley Winkler <rex8112@gmail.com>
2022-06-29 14:33:04 -0500

    Preserve new settings defaults in _deserialize

--- helios/channel.py
+++ helios/channel.py
@@ -68,9 +68,11 @@ class Channel:
 
     def _deserialize(self, data: dict) -> None:
         if self.channel_type != data.get('type'):
-            raise TypeError(f'Channel data is of type {data.get("type")} not {self.channel_type}')
-        self.settings = data.get('settings', self.settings)
+            raise TypeError(f'Channel data is of type `{data.get("type")}` not `{self.channel_type}`')
         self.flags = data.get('flags', self.flags)
+        settings = data.get('settings', {})
+        for k, v in settings.items():
+            self.settings[k] = v
 
     def serialize(self) -> dict:
         return {
