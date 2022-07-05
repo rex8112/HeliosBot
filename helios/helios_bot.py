d96eeb5592e58b0c0b1397016c8b8e920105ec47
Riley Winkler <rex8112@gmail.com>
2022-07-04 21:43:06 -0500

    Move HTTPClient assignment to setup_hook

--- helios/helios_bot.py
+++ helios/helios_bot.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 from discord.ext import commands
 
@@ -14,9 +15,17 @@ class HeliosBot(commands.Bot):
         self.servers = ServerManager(self)
         self.settings = settings
         self.ready_once = True
-        self.http = HTTPClient(self.settings.api_url, self.settings.api_username, self.settings.api_password)
+        self.helios_http: Optional[HTTPClient] = None
 
         super().__init__(command_prefix, intents=intents, **options)
+
+    async def setup_hook(self) -> None:
+        self.helios_http = HTTPClient(
+            self.settings.api_url,
+            loop=self.loop,
+            api_username=self.settings.api_username,
+            api_password=self.settings.api_password
+        )
 
     async def on_ready(self):
         logger.info(f'Logged in as {self.user} (ID: {self.user.id})')
--- helios/http.py
+++ helios/http.py
@@ -1,10 +1,13 @@
-from typing import Union
+from typing import Union, TYPE_CHECKING
 
 import aiohttp
 
+if TYPE_CHECKING:
+    from asyncio import AbstractEventLoop
+
 
 class HTTPClient:
-    def __init__(self, base_url, api_username, api_password):
+    def __init__(self, base_url, *, loop: 'AbstractEventLoop', api_username: str, api_password: str):
         self._session = aiohttp.ClientSession(
             base_url=base_url,
             auth=aiohttp.BasicAuth(api_username, api_password)
@@ -16,7 +19,7 @@ class HTTPClient:
         return await resp.json()
 
     async def get_server(self, guild_id: str):
-        resp = await self.request(f'/servers/{guild_id}/')
+        resp = await self.request(f'servers/{guild_id}/')
         return resp
 
     async def add_server(self, guild_id: str, **data):
@@ -25,11 +28,11 @@ class HTTPClient:
     async def get_channel(self, channel_id: str = None, **params) -> Union[dict, list]:
         resp = None
         if channel_id:
-            resp = await self.request(f'/channels/{channel_id}/')
+            resp = await self.request(f'channels/{channel_id}/')
         else:
-            resp = await self.request(f'/channels/', **params)
+            resp = await self.request(f'channels/', **params)
         return resp
 
     async def put_channel(self, json_data: Union[dict, list]):
-        resp = await self.request('/channels/', method='PUT', json=json_data)
+        resp = await self.request('channels/', method='PUT', json=json_data)
         return resp
