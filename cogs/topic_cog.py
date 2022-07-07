0d0932102c303f3b734e0e58fabd41b6f56c7223
Riley Winkler <rex8112@gmail.com>
2022-07-06 20:39:47 -0500

    Add topic channel creation

--- cogs/topic_cog.py
+++ cogs/topic_cog.py
@@ -1,12 +1,17 @@
-import discord
+from typing import TYPE_CHECKING
 
+import discord
 from discord import app_commands
 from discord.ext import commands
+
 from helios import TopicCreation
+
+if TYPE_CHECKING:
+    from helios import HeliosBot
 
 
 class TopicCog(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: 'HeliosBot'):
         self.bot = bot
 
     @app_commands.command(name='topic', description='Create a new topic')
--- helios/modals.py
+++ helios/modals.py
@@ -1,11 +1,21 @@
+import traceback
+from typing import TYPE_CHECKING
+
 from discord import ui, Interaction
+
+if TYPE_CHECKING:
+    from .server import Server
 
 
 class TopicCreation(ui.Modal, title='New Topic'):
     name = ui.TextInput(label='Name')
 
     async def on_submit(self, interaction: Interaction) -> None:
-        await interaction.response.send_message(f'Wowie', ephemeral=True)
+        bot = interaction.client
+        server: 'Server' = bot.servers.get(interaction.guild_id)
+        result, result_message = await server.channels.create_topic(self.name.value, interaction.user)
+        await interaction.response.send_message(result_message, ephemeral=True)
 
     async def on_error(self, interaction: Interaction, error: Exception) -> None:
+        traceback.print_exc()
         await interaction.response.send_message('Sorry, something went wrong.', ephemeral=True)
