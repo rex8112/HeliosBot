e82c9f5ff423de5efc54776a8e453e7a11228417
Riley Winkler <rex8112@gmail.com>
2022-07-07 22:44:00 -0500

    Add basic settings commands

---
+++ cogs/settings_cog.py
@@ -0,0 +1,34 @@
+from typing import TYPE_CHECKING
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+if TYPE_CHECKING:
+    from helios import HeliosBot
+
+
+class SettingsCog(commands.GroupCog, name='settings'):
+    def __init__(self, bot: 'HeliosBot'):
+        self.bot = bot
+        super().__init__()
+
+    @app_commands.command(name='topiccategory', description='Set the topic category')
+    @app_commands.checks.has_permissions(administrator=True)
+    async def topic_category(self, interaction: discord.Interaction, topic_category: discord.CategoryChannel):
+        server = self.bot.servers.get(guild_id=interaction.guild_id)
+        server.settings['topic_category'] = topic_category.id
+        await server.save()
+        await interaction.response.send_message('Setting Changed', ephemeral=True)
+
+    @app_commands.command(name='archivecategory', description='Set the archive category')
+    @app_commands.checks.has_permissions(administrator=True)
+    async def topic_category(self, interaction: discord.Interaction, archive_category: discord.CategoryChannel):
+        server = self.bot.servers.get(guild_id=interaction.guild_id)
+        server.settings['archive_category'] = archive_category.id
+        await server.save()
+        await interaction.response.send_message('Setting Changed', ephemeral=True)
+
+
+async def setup(bot: 'HeliosBot'):
+    await bot.add_cog(SettingsCog(bot))
