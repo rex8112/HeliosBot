f18f11d0ef3017eee83c2595d3716ae1799ad607
Riley Winkler <rex8112@gmail.com>
2022-07-21 13:57:24 -0500

    Add final message at the end of all the tests

--- tests/manual/test_race.py
+++ tests/manual/test_race.py
@@ -4,6 +4,8 @@ from helios.horses import Horse, RaceHorse, Race
 
 
 async def main():
+    winnings = []
+
     for t in range(1, 6):
         horses = []
         for n in range(6):
@@ -12,6 +14,7 @@ async def main():
         sorted_horses = sorted(horses, key=lambda x: x.quality, reverse=True)
         race_horses = [RaceHorse(h) for h in sorted_horses]
         race = Race(None)
+        race.length = t * 100
         race.horses = race_horses
         print(f'======= Starting Race {t} =======')
         print(race.get_progress_string())
@@ -28,6 +31,18 @@ async def main():
         for i, h in enumerate(race.finished):
             winning += f'{i}: {h.name} - {h.horse.quality} - {h.horse.speed} - {h.horse.acceleration} - {h.horse.stamina}\n'
         print(winning)
+        winnings.append(race.finished)
+    final_message = ''
+    for i in range(len(winnings[0])):
+        line = ''
+        for x in range(len(winnings)):
+            horse = winnings[x][i]
+            if horse.tick_finished:
+                line += f'{horse.name}:{horse.tick_finished:4}:{horse.progress:5.2f} - '
+            else:
+                line += f'{horse.name}: DNF:{horse.progress:6.2f} - '
+        final_message += f'{line}\n'
+    print(final_message)
 
 asyncio.run(main())
 
