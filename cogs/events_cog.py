1eff2898366884bf1e74eeddde9d09b722e83e2b
Riley Winkler <rex8112@gmail.com>
2022-08-09 13:15:54 -0500

    Add pinging voice channel system
    Signed-off-by: Riley Winkler <rex8112@gmail.com>

--- cogs/events_cog.py
+++ cogs/events_cog.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING
 import discord
 from discord.ext import commands
 
+from helios.views import YesNoView
+
 if TYPE_CHECKING:
     from helios import HeliosBot
 
@@ -23,6 +25,31 @@ class EventsCog(commands.Cog):
         server = self.bot.servers.get(guild.id)
         if not server:
             self.bot.servers.add_server(guild)
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.guild.me in message.mentions and message.author.voice.channel:
+            channel = message.author.voice.channel
+            mention_string = ''
+            allowed = []
+            for m in channel.members:
+                mention_string += f'{m.mention} '
+                if m != message.author:
+                    allowed.append(m)
+            view = YesNoView(message.author)
+            mess = await message.channel.send(
+                f'Would you like to ping everyone in {channel.mention}?',
+                view=view,
+                delete_after=5
+            )
+            await view.wait()
+            if view.value:
+                await message.channel.send(mention_string, allowed_mentions=discord.AllowedMentions(users=allowed))
+            else:
+                try:
+                    await mess.delete()
+                except discord.NotFound:
+                    ...
 
 
 async def setup(bot: 'HeliosBot'):
--- helios/views.py
+++ helios/views.py
@@ -2,9 +2,9 @@ from typing import Optional, TYPE_CHECKING
 
 import discord
 
+from .horses.enumerations import BetType
 from .modals import BetModal
 from .types import HeliosChannel
-from .horses.enumerations import BetType
 
 if TYPE_CHECKING:
     from .helios_bot import HeliosBot
@@ -103,3 +103,28 @@ class PreRaceView(discord.ui.View):
         for i, h in enumerate(horses):
             horse_strings.append(f'{i}. {h.name}')
         # TODO: Call register view
+
+
+class YesNoView(discord.ui.View):
+    def __init__(self, author: discord.Member, *, timeout=5):
+        super().__init__(timeout=timeout)
+        self.author = author
+        self.value = None
+
+    @discord.ui.button(label='Yes', style=discord.ButtonStyle.green)
+    async def yes(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user == self.author:
+            self.value = True
+            await interaction.response.defer()
+            self.stop()
+        else:
+            await interaction.response.send_message('You are not allowed to respond', ephemeral=True)
+
+    @discord.ui.button(label='No', style=discord.ButtonStyle.red)
+    async def no(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user == self.author:
+            self.value = False
+            await interaction.response.defer()
+            self.stop()
+        else:
+            await interaction.response.send_message('You are not allowed to respond', ephemeral=True)
