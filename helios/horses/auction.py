ed4a76692c970939e5360285a3bf7a5cfa0634f9
Riley Winkler <rex8112@gmail.com>
2022-08-18 16:42:33 -0500

    Add bid serialization
    Signed-off-by: Riley Winkler <rex8112@gmail.com>

--- helios/horses/auction.py
+++ helios/horses/auction.py
@@ -6,10 +6,40 @@ if TYPE_CHECKING:
 
 
 class Bid:
-    def __init__(self, bidder_id: int, listing_id: int, time: datetime):
+    def __init__(self, bidder_id: int, listing_id: int, amount: int,
+                 time: datetime):
         self.bidder_id = bidder_id
         self.listing_id = listing_id
+        self.amount = amount
         self.time = time
+
+    @classmethod
+    def from_data(cls, data: dict):
+        return cls(
+            bidder_id=data['bidder'],
+            listing_id=data['listing'],
+            amount=data['amount'],
+            time=datetime.fromisoformat(data['time'])
+        )
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, Bid):
+            return (o.bidder_id == self.bidder_id
+                    and o.listing_id == self.listing_id
+                    and o.amount == self.amount)
+        else:
+            return NotImplemented
+
+    def __ne__(self, o: object) -> bool:
+        return not self.__eq__(o)
+
+    def serialize(self):
+        return {
+            'bidder': self.bidder_id,
+            'listing': self.listing_id,
+            'amount': self.amount,
+            'time': self.time.isoformat()
+        }
 
 
 class HorseListing:
