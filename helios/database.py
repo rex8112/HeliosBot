8ed07758fcf2ddfa16127903a9e28b954ca9283d
Riley Winkler <rex8112@gmail.com>
2023-05-23 15:54:58 -0500

    Define models

--- helios/database.py
+++ helios/database.py
@@ -3,3 +3,34 @@ from peewee import *
 db = SqliteDatabase('helios.db')
 
 
+class Server(Model):
+    id = BigIntegerField(primary_key=True, unique=True)
+    name = CharField(max_length=26)
+    settings = TextField(default="")
+    flags = TextField(default="[]")
+
+    class Meta:
+        database = db
+
+
+class Channel(Model):
+    id = BigIntegerField(primary_key=True, unique=True)
+    server = ForeignKeyField(Server, backref='server')
+    type = CharField(max_length=26)
+    settings = TextField(default='')
+    flags = TextField(default='[]')
+
+    class Meta:
+        database = db
+
+
+class Member(Model):
+    id = AutoField(primary_key=True, unique=True)
+    server = ForeignKeyField(Server, backref='server')
+    member_id = BigIntegerField()
+    templates = TextField(default='[]')
+    settings = TextField(default='')
+    flags = TextField(default='[]')
+
+    class Meta:
+        database = db
