3460e295db7929e46e1114ccb589231690087be3
Riley Winkler <rex8112@gmail.com>
2023-06-03 17:20:43 -0500

    Fix not resetting archive message on save

--- helios/channel.py
+++ helios/channel.py
@@ -301,7 +301,7 @@ class TopicChannel(Channel):
             embed = self._get_saved_embed()
             embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
             await interaction.response.edit_message(embed=embed, view=None)
-        self.archive_message_id = None
+        self.archive_message = None
 
     async def evaluate_tier(self, change=True, allow_degrade=False) -> int:
         """
