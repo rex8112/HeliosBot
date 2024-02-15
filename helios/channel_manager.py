599cba99034677578ccd0cea632349da58505847
Riley Winkler <rex8112@gmail.com>
2024-02-14 18:33:08 -0600

    Sort pins based on their current position

--- helios/channel_manager.py
+++ helios/channel_manager.py
@@ -115,7 +115,8 @@ class ChannelManager:
     async def manage_topics(self):
         """Get channel points, sort by them and evaluate_state on the lower channels after the tenth channel."""
         topic_channels = list(filter(lambda x: x.active or x.pending, self.topic_channels.values()))
-        pinned = list(filter(lambda x: x.pinned, self.topic_channels.values()))
+        pinned = list(sorted(filter(lambda x: x.pinned, self.topic_channels.values()),
+                             key=lambda x: x.channel.position))
         if len(topic_channels) == 0:
             return
         e_values = []
